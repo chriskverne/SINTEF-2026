821bfb46ef834fa61a75fe9804b73345737826f3
Christopher Kverne <christopherkverne@Christophers-MacBook-Pro.local>
2026-06-18 14:14:09 +0200

    prepared GBM state through binomial tree and plotted both state dist and X_t distributions matching our GBM log curve

--- Models/Geometric Brownian Motion.py
+++ Models/Geometric Brownian Motion.py
@@ -1,18 +1,21 @@
 import numpy as np
-
+import pennylane as qml
+import matplotlib.pyplot as plt
 
 class GeometricBrownianMotion:
-    def __init__(self, S0, mu, sigma, dt):
+    def __init__(self, S0, mu, sigma, dt, m):
         """
             S0: Initial price
             mu: expected return (drift)
             sigma: standard deviation (volatility)
             dt: time step size
+            m: number of time steps
         """
         self.S0 = S0
         self.mu = mu
         self.sigma = sigma
         self.dt = dt
+        self.m = m
 
         # Compute up/down factors + probabilities + quantum rotation angle
         self.u = np.exp(self.sigma * np.sqrt(self.dt))
@@ -22,36 +25,63 @@ class GeometricBrownianMotion:
         # q = sin^2(theta/2) -> theta = 2 * arcsin(sqrt(q))
         self.theta = 2 * np.arcsin(np.sqrt(self.q))
 
-    def create_quantum_state(self, m):
+    def create_quantum_state(self):
         """
             m: number of qubits (time steps)
             Returns a quantum state representing the price distribution at time T = m*dt
         """
 
-        state = np.zeros(2**m)
-        ry_mat = np.array([
-            [np.cos(self.theta/2), -np.sin(self.theta/2)],
-            [np.sin(self.theta/2), np.cos(self.theta/2)]
-        ])
+        # Creates quantum state with m qubits
+        dev = qml.device('default.qubit', wires=self.m)
+        @qml.qnode(dev)
 
-        for i in range(m):
-            ,,,, (Not sure where to continue)
+        def preparation():
+            for i in range(self.m):
+                qml.RY(self.theta, wires=i)
+            
+            return qml.state()
         
-        return None
+        return preparation()
+    
+    def plot_state_distribution(self, state):
+        probs = np.abs(state)**2 
 
-    def build_binomial_tree(self, m):
+        # We only have m+1 outcomes as ddu = dud = udd
+        outcomes = np.zeros(self.m + 1)
+
+        # Iterate over state and count no of 1's. We use this as an index in outcomes
+        for i in range(len(probs)):
+            outcomes[bin(i).count('1')] += probs[i]
+
+        plt.bar(np.arange(self.m + 1), outcomes)
+        plt.xticks(np.arange(self.m + 1), [f'{i} up' for i in range(self.m + 1)])
+        plt.show()
+
+    def plot_prices(self, state):
+        probs = np.abs(state)**2 
+
+        # We only have m+1 outcomes as ddu = dud = udd
+        outcomes = np.zeros(self.m + 1)
+
+        # Iterate over state and count no of 1's. We use this as an index in outcomes
+        for i in range(len(probs)):
+            outcomes[bin(i).count('1')] += probs[i]
+
+        plt.bar(np.arange(self.m + 1), outcomes)
+        plt.xticks(np.arange(self.m + 1), [f'{self.S0 * self.u**i * self.d**(self.m - i):.2f}$' for i in range(self.m + 1)])
+        plt.show()
+    
+    def single_qubit_amplitude(self, qubit_index):
         """
-            m: number of time steps
-            Builds a binomial tree for the underlying asset price evolution
-            Will be the classical formulation of function above
+            Add a single qubit to prepare the state (sqrt(1-p) |0> + sqrt(p) |1>) where |0>/|1> represents when a condition is met
         """
-        return None
 
 
-mean = 0
-std = 1
-expected_return = 0.04
-
-dW_t = np.random.normal(loc=mean, scale=std,size = 100) 
-
-print(dW_t)
+std = 0.20
+expected_return = 0.08
+step_size = 1.0
+m_qubits = 15
+GBM = GeometricBrownianMotion(S0=100, mu=expected_return, sigma=std, dt=step_size, m=m_qubits)
+get_state = GBM.create_quantum_state()
+GBM.plot_prices(get_state)
+GBM.plot_state_distribution(get_state)
